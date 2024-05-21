3f4ce605b3b2d26444114438f9688b5a85164ddd
Joao <joao.rampoo@gmail.com>
2024-05-21 17:32:38 -0300

    base start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <h3>Altura:</h3>
+          <input type="number" />
+        </div>
+        <div>
+          <h3>Peso:</h3>
+          <input type="number" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
