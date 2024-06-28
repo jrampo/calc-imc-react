55f9411d1a5ec25f631233b30c22708ccb80541d
Joao <joao.rampoo@gmail.com>
2024-06-28 16:08:16 +0000

    new layout + responsive

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -21,52 +21,54 @@ const IMCCalculator = () => {
   return (
     <div>
       <h1>Calculadora de IMC</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <div>
+        <form className="card" onSubmit={handleSubmit}>
           <div>
-            <label>
-              Peso:
-              <input
-                onChange={(e) => setPeso(e.target.value)}
-                value={peso}
-                type="number"
-                required
-              />
-            </label>
+            <div>
+              <label>
+                Peso:
+                <input
+                  onChange={(e) => setPeso(e.target.value)}
+                  value={peso}
+                  type="number"
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Altura em centimetros:
+                <input
+                  onChange={(e) => setAltura(e.target.value)}
+                  value={altura}
+                  type="number"
+                  required
+                />
+              </label>
+            </div>
           </div>
-          <div>
-            <label>
-              Altura em centimetros:
-              <input
-                onChange={(e) => setAltura(e.target.value)}
-                value={altura}
-                type="number"
-                required
-              />
-            </label>
+          <button type="submit">+</button>
+        </form>
+        {imc && (
+          <div className="IMCmargin">
+            <h2>Seu IMC: {imc}</h2>
+            <p className="imc">
+              {imc < 18.5
+                ? "Abaixo do peso"
+                : imc < 24.9
+                ? "Peso normal"
+                : imc < 29.9
+                ? "Sobrepeso"
+                : imc < 34.9
+                ? "Obesidade grau I"
+                : imc < 39.9
+                ? "Obesidade grau II"
+                : "Obesidade grau III"}
+            </p>
+            <img className="imc-img" src={IMC} alt="Tabela IMC" />
           </div>
-        </div>
-        <button type="submit">+</button>
-      </form>
-      {imc && (
-        <div>
-          <h2>Seu IMC: {imc}</h2>
-          <p className="imc">
-            {imc < 18.5
-              ? "Abaixo do peso"
-              : imc < 24.9
-              ? "Peso normal"
-              : imc < 29.9
-              ? "Sobrepeso"
-              : imc < 34.9
-              ? "Obesidade grau I"
-              : imc < 39.9
-              ? "Obesidade grau II"
-              : "Obesidade grau III"}
-          </p>
-          <img className="imc-img" src={IMC} alt="Tabela IMC" />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
